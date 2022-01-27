2c0b498676b80126ec3214a5becc743fbf974251
Lucas Santana <lucas_377@hotmail.com>
2022-01-26 21:15:57 -0300

    Adicionado gitignore

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Bem vindo!</h1>
+            <h2>Messenger Chat - Matrix</h2>
+        </div>
+    )
+}
+
+export default HomePage
